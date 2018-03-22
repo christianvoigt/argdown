3f888b885f3e1224d20b262aa184f5fc82100b11
Christian Voigt <1pxsolidblack@googlemail.com>
2018-03-22 11:21:25 +0100

    Rewrite of SvgToPngExportPlugin, using sharp image converter.

--- packages/argdown-cli/lib/src/commands/MapCommand.js
+++ packages/argdown-cli/lib/src/commands/MapCommand.js
@@ -1,199 +1,206 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 exports.handler = exports.builder = exports.desc = exports.command = undefined;
 
-var _index = require('../index.js');
+var _index = require("../index.js");
 
-const command = exports.command = 'map [inputGlob] [outputDir]';
-const desc = exports.desc = 'export Argdown input as DOT files';
+const command = exports.command = "map [inputGlob] [outputDir]";
+const desc = exports.desc = "export Argdown input as DOT files";
 const builder = exports.builder = {
-  logParserErrors: {
-    alias: 'e',
-    describe: 'Log parser errors to console',
-    type: 'boolean',
-    default: true
-  },
-  useHtmlLabels: {
-    alias: 'html-labels',
-    describe: 'Use HTML node labels',
-    type: 'boolean'
-  },
-  argumentLabelMode: {
-    alias: 'argument-labels',
-    choices: [undefined, 'hide-untitled', 'title', 'description'],
-    type: 'string',
-    describe: 'The method by which argument label content is selected'
-  },
-  statementLabelMode: {
-    alias: 'statement-labels',
-    choices: [undefined, 'hide-untitled', 'title', 'text'],
-    type: 'string',
-    describe: 'The method by which statement label content is selected'
-  },
-  statementSelectionMode: {
-    alias: 'statement-selection',
-    type: 'string',
-    choices: [undefined, 'all', 'titled', 'roots', 'statement-trees', 'with-relations']
-  },
-  graphName: {
-    alias: 'name',
-    type: 'string',
-    describe: 'Name of the graph'
-  },
-  lineLength: {
-    alias: 'line',
-    type: 'number',
-    describe: 'Number of chars in a label line.'
-  },
-  groupColors: {
-    type: 'array',
-    describe: 'Colors for groups sorted by stacking order'
-  },
-  inclusive: {
-    type: 'boolean',
-    describe: 'Include disconnected nodes.'
-  },
-  rankdir: {
-    type: 'string',
-    describe: 'Graphviz rankdir setting'
-  },
-  concentrate: {
-    type: 'string',
-    describe: 'Graphviz concentrate setting'
-  },
-  ratio: {
-    type: 'string',
-    describe: 'Graphviz ratio setting'
-  },
-  size: {
-    type: 'string',
-    describe: 'Graphviz size setting'
-  },
-  format: {
-    alias: 'f',
-    type: 'string',
-    describe: 'the file format (dot, svg, pdf, png)',
-    default: 'pdf'
-  },
-  width: {
-    alias: 'w',
-    type: 'number',
-    describe: 'the width of the png image (only used if format is png)'
-  },
-  height: {
-    alias: 'h',
-    type: 'number',
-    describe: 'the height of the png image (only used if format is png)'
-  }
+    logParserErrors: {
+        alias: "e",
+        describe: "Log parser errors to console",
+        type: "boolean",
+        default: true
+    },
+    useHtmlLabels: {
+        alias: "html-labels",
+        describe: "Use HTML node labels",
+        type: "boolean"
+    },
+    argumentLabelMode: {
+        alias: "argument-labels",
+        choices: [undefined, "hide-untitled", "title", "description"],
+        type: "string",
+        describe: "The method by which argument label content is selected"
+    },
+    statementLabelMode: {
+        alias: "statement-labels",
+        choices: [undefined, "hide-untitled", "title", "text"],
+        type: "string",
+        describe: "The method by which statement label content is selected"
+    },
+    statementSelectionMode: {
+        alias: "statement-selection",
+        type: "string",
+        choices: [undefined, "all", "titled", "roots", "statement-trees", "with-relations"]
+    },
+    graphName: {
+        alias: "name",
+        type: "string",
+        describe: "Name of the graph"
+    },
+    lineLength: {
+        alias: "line",
+        type: "number",
+        describe: "Number of chars in a label line."
+    },
+    groupColors: {
+        type: "array",
+        describe: "Colors for groups sorted by stacking order"
+    },
+    inclusive: {
+        type: "boolean",
+        describe: "Include disconnected nodes."
+    },
+    rankdir: {
+        type: "string",
+        describe: "Graphviz rankdir setting"
+    },
+    concentrate: {
+        type: "string",
+        describe: "Graphviz concentrate setting"
+    },
+    ratio: {
+        type: "string",
+        describe: "Graphviz ratio setting"
+    },
+    size: {
+        type: "string",
+        describe: "Graphviz size setting"
+    },
+    format: {
+        alias: "f",
+        type: "string",
+        describe: "the file format (dot, svg, pdf, png)",
+        default: "pdf"
+    },
+    width: {
+        type: "number",
+        describe: "the width of the png image (only used if format is png)"
+    },
+    height: {
+        type: "number",
+        describe: "the height of the png image (only used if format is png)"
+    },
+    density: {
+        alias: "d",
+        type: "number",
+        describe: "the dpi density of the png image (default is 72dpi)"
+    }
 };
 const handler = exports.handler = function (argv) {
-  let config = _index.app.loadConfig(argv.config);
+    let config = _index.app.loadConfig(argv.config);
 
-  config.dot = config.dot || config.DotExport || {};
-  config.map = config.map || config.MapMaker || {};
-  const format = argv.format || 'pdf';
-  if (format === 'pdf') {
-    config.svgToPdf = config.svgToPdf || config.SvgToPdfExportPlugin || {};
-  } else if (format === 'png') {
-    config.svgToPng = config.svgToPng || config.SvgToPngExportPlugin || {};
-    if (argv.width) {
-      config.svgToPng.width = argv.width;
-    }
-    if (argv.height) {
-      config.svgToPng.height = argv.height;
-    }
-  } else {
-    config.saveAs = config.saveAs || config.SaveAsFilePlugin || {};
-  }
-
-  if (argv.useHtmlLabels) {
-    config.dot.useHtmlLabels = true;
-  }
-
-  if (argv.argumentLabelMode) {
-    config.dot.argumentLabelMode = argv.argumentLabelMode;
-  }
-  if (argv.statementLabelMode) {
-    config.dot.statementLabelMode = argv.statementLabelMode;
-  }
-  if (argv.statementSelectionMode) {
-    config.map.statementSelectionMode = argv.statementSelectionMode;
-  }
-  if (argv.inclusive) {
-    config.map.excludeDisconnected = false;
-  }
-
-  if (argv.graphName) {
-    config.dot.graphname = argv.graphName;
-  }
-  if (argv.lineLength) {
-    config.dot.lineLength = argv.lineLength;
-  }
-  if (argv.groupColors) {
-    config.dot.groupColors = argv.groupColors;
-  }
-
-  config.dot.graphVizSettings = config.dot.graphVizSettings || {};
-  if (argv.concentration) {
-    config.dot.graphVizSettings.concentration = argv.contentration;
-  }
-  if (argv.size) {
-    config.dot.graphVizSettings.size = argv.size;
-  }
-  if (argv.ratio) {
-    config.dot.graphVizSettings.ratio = argv.ratio;
-  }
-  if (argv.rankdir) {
-    config.dot.graphVizSettings.rankdir = argv.rankdir;
-  }
-
-  if (argv.inputGlob) {
-    config.inputPath = argv.inputGlob;
-  }
-  if (argv.outputDir) {
-    if (format === 'pdf') {
-      config.svgToPdf.outputDir = argv.outputDir;
-    } else if (format === 'png') {
-      config.svgToPng.outputDir = argv.outputDir;
+    config.dot = config.dot || config.DotExport || {};
+    config.map = config.map || config.MapMaker || {};
+    const format = argv.format || "pdf";
+    console.log("argv.width: " + argv.width);
+    if (format === "pdf") {
+        config.svgToPdf = config.svgToPdf || config.SvgToPdfExportPlugin || {};
+    } else if (format === "png") {
+        config.svgToPng = config.svgToPng || config.SvgToPngExportPlugin || {};
+        if (argv.width) {
+            config.svgToPng.width = argv.width;
+        }
+        if (argv.height) {
+            config.svgToPng.height = argv.height;
+        }
+        if (argv.density) {
+            config.svgToPng.density = argv.density;
+        }
     } else {
-      config.saveAs.outputDir = argv.outputDir;
+        config.saveAs = config.saveAs || config.SaveAsFilePlugin || {};
     }
-  }
-  config.logLevel = argv.verbose ? "verbose" : config.logLevel;
-  config.watch = argv.watch || config.watch;
-  config.process = ['preprocessor', 'parse-input'];
-  config.logParserErrors = argv.logParserErrors || config.logParserErrors;
-  if (config.logParserErrors) {
-    config.process.push("log-parser-errors");
-  }
-  config.process.push('build-model');
-  config.process.push('export-dot');
-  if (format !== 'dot') {
-    config.process.push('export-svg');
-  }
-  if (!argv.stdout || argv.outputDir) {
-    if (format === 'dot') {
-      config.process.push('save-as-dot');
-    } else if (format === 'svg') {
-      config.process.push('save-svg-as-svg');
-    } else if (format === 'png') {
-      config.process.push('save-svg-as-png');
-    } else {
-      config.process.push('save-svg-as-pdf');
-    }
-  }
 
-  if (argv.stdout) {
-    if (format === 'dot') {
-      config.process.push('stdout-dot');
-    } else {
-      // pdf and png to stdout is currently not supported
-      config.process.push('stdout-svg');
+    if (argv.useHtmlLabels) {
+        config.dot.useHtmlLabels = true;
     }
-  }
-  _index.app.load(config).catch(e => console.log(e));
+
+    if (argv.argumentLabelMode) {
+        config.dot.argumentLabelMode = argv.argumentLabelMode;
+    }
+    if (argv.statementLabelMode) {
+        config.dot.statementLabelMode = argv.statementLabelMode;
+    }
+    if (argv.statementSelectionMode) {
+        config.map.statementSelectionMode = argv.statementSelectionMode;
+    }
+    if (argv.inclusive) {
+        config.map.excludeDisconnected = false;
+    }
+
+    if (argv.graphName) {
+        config.dot.graphname = argv.graphName;
+    }
+    if (argv.lineLength) {
+        config.dot.lineLength = argv.lineLength;
+    }
+    if (argv.groupColors) {
+        config.dot.groupColors = argv.groupColors;
+    }
+
+    config.dot.graphVizSettings = config.dot.graphVizSettings || {};
+    if (argv.concentration) {
+        config.dot.graphVizSettings.concentration = argv.contentration;
+    }
+    if (argv.size) {
+        config.dot.graphVizSettings.size = argv.size;
+    }
+    if (argv.ratio) {
+        config.dot.graphVizSettings.ratio = argv.ratio;
+    }
+    if (argv.rankdir) {
+        config.dot.graphVizSettings.rankdir = argv.rankdir;
+    }
+
+    if (argv.inputGlob) {
+        config.inputPath = argv.inputGlob;
+    }
+    if (argv.outputDir) {
+        if (format === "pdf") {
+            config.svgToPdf.outputDir = argv.outputDir;
+        } else if (format === "png") {
+            config.svgToPng.outputDir = argv.outputDir;
+        } else {
+            config.saveAs.outputDir = argv.outputDir;
+        }
+    }
+    config.logLevel = argv.verbose ? "verbose" : config.logLevel;
+    config.watch = argv.watch || config.watch;
+    config.process = ["preprocessor", "parse-input"];
+    config.logParserErrors = argv.logParserErrors || config.logParserErrors;
+    if (config.logParserErrors) {
+        config.process.push("log-parser-errors");
+    }
+    config.process.push("build-model");
+    config.process.push("export-dot");
+    if (format !== "dot") {
+        config.process.push("export-svg");
+    }
+    if (!argv.stdout || argv.outputDir) {
+        if (format === "dot") {
+            config.process.push("save-as-dot");
+        } else if (format === "svg") {
+            config.process.push("save-svg-as-svg");
+        } else if (format === "png") {
+            config.process.push("save-svg-as-png");
+        } else {
+            config.process.push("save-svg-as-pdf");
+        }
+    }
+
+    if (argv.stdout) {
+        if (format === "dot") {
+            config.process.push("stdout-dot");
+        } else {
+            // pdf and png to stdout is currently not supported
+            config.process.push("stdout-svg");
+        }
+    }
+    _index.app.load(config).catch(e => console.log(e));
 };
 //# sourceMappingURL=MapCommand.js.map
--- packages/argdown-cli/src/plugins/SvgToPngExportPlugin.js
+++ packages/argdown-cli/src/plugins/SvgToPngExportPlugin.js
@@ -1,21 +1,19 @@
-// We have to use a local file and let babel ignore it, until pdfkit is ported to ES6
-const svg2png = require("svg2png");
-var fs = require('fs');
-let path = require('path');
-let mkdirp = require('mkdirp');
-import * as _ from 'lodash';
+import * as _ from "lodash";
+const sharp = require("sharp");
+let mkdirp = require("mkdirp");
 
 class SvgToPngExportPlugin {
     set config(config) {
         let previousSettings = this.settings;
         if (!previousSettings) {
             previousSettings = {
-                outputDir: "./png"
+                outputDir: "./png",
+                density: 300
             };
         }
         this.settings = _.defaultsDeep({}, config, previousSettings);
         // enforce svg export
-        this.settings.format = 'png';
+        this.settings.format = "png";
     }
     constructor(config) {
         this.name = "SvgToPngExportPlugin";
@@ -30,7 +28,7 @@ class SvgToPngExportPlugin {
         if (!response.svg) {
             return response;
         }
-        let fileName = 'default';
+        let fileName = "default";
         if (_.isFunction(this.settings.fileName)) {
             fileName = this.settings.fileName.call(this, request, response);
         } else if (_.isString(this.settings.fileName)) {
@@ -38,41 +36,77 @@ class SvgToPngExportPlugin {
         } else if (request.inputPath) {
             fileName = this.getFileName(request.inputPath);
         }
-        const absoluteOutputDir = path.resolve(process.cwd(), this.settings.outputDir);
-        const filePath = absoluteOutputDir + '/' + fileName + '.png';
+        const absoluteOutputDir = path.resolve(
+            process.cwd(),
+            this.settings.outputDir
+        );
+        const filePath = absoluteOutputDir + "/" + fileName + ".png";
         const settings = this.settings;
-        await new Promise((resolve, reject)=>{
-            mkdirp(absoluteOutputDir, function (err) {
+
+        // Graphviz svg font sizes are missing a size format (font-size="10.00") and are interpreted by sharp (librsvg) as pixel sizes.
+        // Because of this if density is increased, Sharp (librsvg) will not scale the text with the rest of the image.
+        // To avoid this we have to change all font-sizes to explicit point sizes.
+        // Normally 10px should be converted to 7.5pt and 8px should be converted to 6pt
+        // For some unknown reason this leads to wrong results
+        // Instead 10px has to be convertd to 9pt and 8px has to be converted to 7.5pt to obtain satisfactory results.
+        // This will break if font sizes or if the viz.js svg output change. :(
+        let convertedSvg = response.svg;
+        convertedSvg = this.replaceAll(
+            convertedSvg,
+            'font-size="10.00"',
+            'font-size="9pt"'
+        );
+        convertedSvg = this.replaceAll(
+            convertedSvg,
+            'font-size="8.00"',
+            'font-size="7.5pt"'
+        );
+
+        await new Promise((resolve, reject) => {
+            mkdirp(absoluteOutputDir, function(err) {
                 if (err) {
                     reject(err);
                 }
                 resolve();
             });
         });
-        const options = {};
-        if (settings.width) {
-            options.width = settings.width;
+        if (settings.width && settings.height) {
+            await sharp(new Buffer(convertedSvg), { density: settings.density })
+                .resize(settings.width, settings.height)
+                .max()
+                .png()
+                .toFile(filePath);
+        } else {
+            await sharp(new Buffer(convertedSvg), { density: settings.density })
+                .png()
+                .toFile(filePath);
         }
-        if (settings.height) {
-            options.height = settings.height;
-        }
-        const outputBuffer = await svg2png(response.svg, options);
-        await new Promise((resolve, reject)=>{
-            fs.writeFile(filePath, outputBuffer, function (err) {
-                if (err) {
-                    reject(err);
-                }
-                logger.log("verbose", "Saved " + filePath);
-                resolve();
-            });
-        });
+        // const browser = await puppeteer.launch();
+        // const page = await browser.newPage();
+        // const htmlString = `<html><head></head><body><div id='map'>${
+        //     response.svg
+        // }</div></body></html>`;
+        // await page.setContent(htmlString);
+
+        // const rect = await page.evaluate(selector => {
+        //     const element = document.querySelector("#map");
+        //     if (!element) return null;
+        //     return element.getBoundingClientRect();
+        // }, selector);
+        // await page.screenshot({
+        //     path: filePath,
+        //     clip: rect
+        // });
         return response;
     }
     getFileName(file) {
         let extension = path.extname(file);
         return path.basename(file, extension);
     }
+    replaceAll(str, find, replace) {
+        return str.replace(new RegExp(find, "g"), replace);
+    }
 }
 module.exports = {
     SvgToPngExportPlugin: SvgToPngExportPlugin
-}
+};
