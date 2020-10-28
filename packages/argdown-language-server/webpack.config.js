6b27190f10ce2c048ea6e5811b5f696a9c72dfc5
Christian Voigt <1pxsolidblack@gmail.com>
2020-10-28 15:27:27 +0100

    fix(@argdown/language-server): updating @argdown/node to v1.5.3 to fix #193

--- packages/argdown-language-server/webpack.config.js
+++ packages/argdown-language-server/webpack.config.js
@@ -1,7 +1,4 @@
 // Using PDFKit fix from here: https://github.com/Pzixel/PDFKit-example/blob/master/webpack.config.js
-
-const StringReplacePlugin = require("string-replace-webpack-plugin");
-
 //@ts-check
 
 ("use strict");
@@ -14,27 +11,28 @@ const config = {
 
   entry: "./src/server.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
-    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, "dist"),
     filename: "server.js",
     libraryTarget: "commonjs2",
     devtoolModuleFilenameTemplate: "../[resource-path]"
   },
   devtool: "source-map",
   externals: {
-    vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: [".ts", ".js"],
+    extensions: [".ts", ".js", ".mjs"],
     alias: {
       "unicode-properties": "unicode-properties/unicode-properties.cjs.js",
       pdfkit: "pdfkit/js/pdfkit.js"
     }
   },
-  plugins: [new StringReplacePlugin()],
   module: {
     rules: [
+      {
+        test: /\.mjs$/,
+        type: 'javascript/auto',
+    },
       {
         test: /\.ts$/,
         exclude: /node_modules/,
@@ -54,31 +52,39 @@ const config = {
         use: ["style-loader", "css-loader"]
       },
       {
-        enforce: "pre",
         test: /unicode-properties[\/\\]unicode-properties/,
-        loader: StringReplacePlugin.replace({
-          replacements: [
-            {
-              pattern: "var fs = _interopDefault(require('fs'));",
-              replacement: function() {
-                return "var fs = require('fs');";
-              }
-            }
-          ]
-        })
+        loader: 'string-replace-loader',
+        options: {
+          search: "var fs = _interopDefault(require('fs'));",
+          replace: "var fs = require('fs');",
+        },
+        enforce: "pre"
       },
       {
         test: /unicode-properties[\/\\]unicode-properties/,
-        loader: "transform-loader?brfs"
+        loader: "transform-loader",
+        options:{
+          brfs: true
+        }
       },
-      { test: /pdfkit[/\\]js[/\\]/, loader: "transform-loader?brfs" },
-      { test: /fontkit[\/\\]index.js$/, loader: "transform-loader?brfs" },
+      { test: /pdfkit[/\\]js[/\\]/, loader: "transform-loader",
+      options:{
+        brfs: true
+      } },
+      { test: /fontkit[\/\\]index.js$/, loader: "transform-loader",
+      options:{
+        brfs: true
+      } },
       {
         test: /linebreak[\/\\]src[\/\\]linebreaker.js/,
-        loader: "transform-loader?brfs"
+        loader: "transform-loader",
+        options:{
+          brfs: true
+        }
       }
     ]
-  }
+  },
+  experiments: { asyncWebAssembly: true }
 };
 
 module.exports = config;
